0a59df6d9b9ca73ce7b054a0979b580dcf542b10
trxsrn <trixielavadiasoriano@gmail.com>
2023-06-18 13:15:56 +0800

    Update changes

--- users/admin/assign.php
+++ users/admin/assign.php
@@ -13,34 +13,76 @@ $discipline = $row['discipline'];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $reviewerIds = $_POST['reviewer'];
 
+    // Retrieve assigned reviewers for the paper
+    $assignedReviewersResult = mysqli_query($conn, "SELECT reviewer FROM papers WHERE id = $id");
+    $assignedReviewersRow = mysqli_fetch_assoc($assignedReviewersResult);
+    $assignedReviewers = explode(';', $assignedReviewersRow['reviewer']);
+    $assignedReviewers = array_filter($assignedReviewers);
+
     // Assign reviewers to the paper
-    $count = 0; // Counter for assigned reviewers
+    $count = count($assignedReviewers); // Counter for assigned reviewers
+    $maxReviewers = 5; // Maximum number of reviewers allowed
+
     foreach ($reviewerIds as $reviewerId) {
-        if ($count >= 5) {
-            break; // Limit to 5 reviewers
+        if ($count >= $maxReviewers) {
+            echo "
+            <script>
+                $(document).ready(function() {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Limit Reached',
+                        text: 'You have reached the limit for assigning reviewers.',
+                        timer: 1000,
+                        showConfirmButton: false
+                    }).then(function() {
+                        window.location.href = 'assign.php?id=" . $id . "';
+                    });
+                });
+            </script>";
+            break;
         }
 
-        // Retrieve reviewer details from the reviewer_profile table
-        $result = mysqli_query($conn, "SELECT * FROM reviewer_profile WHERE id_number = '$reviewerId'");
-        $reviewer = mysqli_fetch_assoc($result);
+        if (!in_array($reviewerId, $assignedReviewers)) {
+            // Retrieve reviewer details from the reviewer_profile table
+            $result = mysqli_query($conn, "SELECT * FROM reviewer_profile WHERE id_number = '$reviewerId'");
+            $reviewer = mysqli_fetch_assoc($result);
 
-        if ($reviewer) {
-            $paperId = $_POST['paper_id'];
+            if ($reviewer) {
+                $paperId = $_POST['paper_id'];
 
-            // Update the paper in the database with the assigned reviewer's id_number
-            $updateQuery = "UPDATE papers SET reviewer = '{$reviewer['id_number']}' WHERE id = $paperId";
-            mysqli_query($conn, $updateQuery);
+                // Update the paper in the database with the assigned reviewer's id_number
+                $updateQuery = "UPDATE papers 
+                                SET reviewer = CONCAT(IFNULL(reviewer, ''), '{$reviewer['id_number']};') 
+                                WHERE id = $paperId";
+                mysqli_query($conn, $updateQuery);
 
-            $count++; // Increment the counter
+                $count++; // Increment the counter
+            }
         }
     }
-    header("Location: assign.php");
-    exit();
+
+    // SweetAlert notification for successful assignment
+    echo "
+    <script>
+        $(document).ready(function() {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success!',
+                text: 'The reviewers have been assigned successfully.',
+                timer: 1000,
+                showConfirmButton: false
+            }).then(function() {
+                window.location.href = 'assign.php?id=" . $id . "';
+            });
+        });
+    </script>";
 }
 
 // Retrieve assigned reviewers for the paper
-$assignedReviewers = mysqli_query($conn, "SELECT DISTINCT reviewer FROM papers WHERE id = $id");
-
+$assignedReviewersResult = mysqli_query($conn, "SELECT reviewer FROM papers WHERE id = $id");
+$assignedReviewersRow = mysqli_fetch_assoc($assignedReviewersResult);
+$assignedReviewers = explode(';', $assignedReviewersRow['reviewer']);
+$assignedReviewers = array_filter($assignedReviewers);
 ?>
 
 <!DOCTYPE html>
@@ -59,6 +101,7 @@ $assignedReviewers = mysqli_query($conn, "SELECT DISTINCT reviewer FROM papers W
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
     <link rel="stylesheet" href="css/paper.css">
     <link rel="stylesheet" href="css/navigation.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 
 <body>
@@ -69,7 +112,7 @@ $assignedReviewers = mysqli_query($conn, "SELECT DISTINCT reviewer FROM papers W
             <h6><?php echo $discipline; ?></h6>
             <form method="POST">
                 <input type="hidden" name="paper_id" value="<?php echo $id; ?>">
-                <table class="table table-bordered table-stripped table-hover">
+                <table class="table table-bordered table-striped table-hover">
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -107,9 +150,9 @@ $assignedReviewers = mysqli_query($conn, "SELECT DISTINCT reviewer FROM papers W
 
                         while ($row = $sql->fetch_assoc()) {
                             // Check if the reviewer is already assigned to the paper
-                            $isAssigned = mysqli_query($conn, "SELECT * FROM papers WHERE id = $id AND reviewer = '{$row['id_number']}'");
+                            $isAssigned = in_array($row['id_number'], $assignedReviewers);
 
-                            if (mysqli_num_rows($isAssigned) > 0) {
+                            if ($isAssigned) {
                                 $buttonText = 'ASSIGNED';
                                 $buttonClass = 'btn btn-success';
                             } else {
