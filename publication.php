c8f50fa6419905ae54a01087253c663c3d90db88
trxsrn <trixielavadiasoriano@gmail.com>
2023-06-30 11:41:52 +0800

    Update changes

--- publication.php
+++ publication.php
@@ -64,58 +64,58 @@
 
                 </p>
 
-                <li><b>Maintain Confidentiality</b>: Any papers submitted for review must be regarded as confidential. They cannot be shown to or discussed with others without the editor's permission.</li>
+                <li class="responsibility"><b>Maintain Confidentiality</b>: Any papers submitted for review must be regarded as confidential. They cannot be shown to or discussed with others without the editor's permission.</li>
 
-                <li><b>Evaluate Research Design</b>: The reviewer must evaluate the research design, methods, and procedures to ensure they are appropriate and well-executed. They must also assess the data quality and the statistical analyses' suitability.</li>
+                <li class="responsibility"><b>Evaluate Research Design</b>: The reviewer must evaluate the research design, methods, and procedures to ensure they are appropriate and well-executed. They must also assess the data quality and the statistical analyses' suitability.</li>
 
-                <li><b>Assess Significance</b>: The reviewer must assess the research's importance and relevance to the field. The reviewer must also carefully weigh the findings' significance and potential implications for future study.</li>
+                <li class="responsibility"><b>Assess Significance</b>: The reviewer must assess the research's importance and relevance to the field. The reviewer must also carefully weigh the findings' significance and potential implications for future study.</li>
 
-                <li><b>Provide Constructive Feedback</b>: Provide detailed and constructive feedback to the authors, highlighting the manuscript's strengths and weaknesses. Suggestions for improvement should be specific, actionable, and aimed at improving the quality of the research.</li>
+                <li class="responsibility"><b>Provide Constructive Feedback</b>: Provide detailed and constructive feedback to the authors, highlighting the manuscript's strengths and weaknesses. Suggestions for improvement should be specific, actionable, and aimed at improving the quality of the research.</li>
 
-                <li><b>Fairness and Integrity</b>: A reviewer's decision should be based on their thorough and honest assessment of the manuscript to assure the quality and validity of the presented research. By performing their duties as a reviewer with fairness and integrity, they can assist in preserving the high research standards in your field.</li>
+                <li class="responsibility"><b>Fairness and Integrity</b>: A reviewer's decision should be based on their thorough and honest assessment of the manuscript to assure the quality and validity of the presented research. By performing their duties as a reviewer with fairness and integrity, they can assist in preserving the high research standards in your field.</li>
 
             <h2 class="sub-headers" id="B2">EDITOR RESPONSIBILITIES</h2>
                 <p class="details">
                     The primary responsibility of an editor is to maintain TRR’s academic journal standards by ensuring the quality and validity of the research presented. The manuscript must be thoroughly reviewed to discover any shortcomings in study design, methodology, data analysis, and interpretation. It's also important to assess the research's importance, originality, and relevance.
                 </p>
-                <li><b>Make Decisions</b>: The editor shall decide whether a work will be published or not. They must consider the research's quality, relevance to the field, and potential impact.</li>
+                <li class="responsibility"><b>Make Decisions</b>: The editor shall decide whether a work will be published or not. They must consider the research's quality, relevance to the field, and potential impact.</li>
 
-                <li><b>Review Manuscripts</b>: The editor is in charge of reviewing submitted manuscripts to make sure that it fits the journal's requirements and follows the submission rules.</li>
+                <li class="responsibility"><b>Review Manuscripts</b>: The editor is in charge of reviewing submitted manuscripts to make sure that it fits the journal's requirements and follows the submission rules.</li>
 
-                <li><b>Quality Check</b>: The editor is responsible for guaranteeing that the published study is of good quality. It includes confirming that the research is appropriately designed, conducted, and reported.</li>
+                <li class="responsibility"><b>Quality Check</b>: The editor is responsible for guaranteeing that the published study is of good quality. It includes confirming that the research is appropriately designed, conducted, and reported.</li>
 
-                <li><b>Manage Peer Review</b>: The TRR editor is responsible for running the peer review process, which includes choosing readers, making sure reviews are done on time and thoroughly, and making decisions based on what reviewers say.</li>
+                <li class="responsibility"><b>Manage Peer Review</b>: The TRR editor is responsible for running the peer review process, which includes choosing readers, making sure reviews are done on time and thoroughly, and making decisions based on what reviewers say.</li>
 
-                <li><b>Promote the Journal</b>: The editor is responsible for promoting the publication and its content to potential authors and consumers. It means going to workshops, keeping an online presence, and looking for ways to collaborate and form partnerships.</li>
+                <li class="responsibility"><b>Promote the Journal</b>: The editor is responsible for promoting the publication and its content to potential authors and consumers. It means going to workshops, keeping an online presence, and looking for ways to collaborate and form partnerships.</li>
 
                 <h2 class="sub-headers" id="B3">PUBLISHER RESPONSIBILITIES</h2>
                 <p class="details">
                     A publisher's main responsibility is to verify the manuscripts in the publication manual. This involves meticulously evaluating the article to discover issues with the study's design, methods, data analysis, and interpretation. To meet publication standards, the research must be significant, original, and relevant to the field.
                 </p>
                 
-                <li><b>Set Editorial Policies and Procedures</b>: The publisher is in charge of setting the journal's editorial policies and procedures, such as the rules for submitting papers, having them reviewed by peers, and getting them published.</li>
+                <li class="responsibility"><b>Set Editorial Policies and Procedures</b>: The publisher is in charge of setting the journal's editorial policies and procedures, such as the rules for submitting papers, having them reviewed by peers, and getting them published.</li>
 
                 
-                <li><b>Editorial Board Management</b>: Publishers manage the editorial board, including hiring members, setting policies, and verifying that they comply with industry standards. It requires working with the board to identify areas for development and developing training programs to improve their skills.</li>
+                <li class="responsibility"><b>Editorial Board Management</b>: Publishers manage the editorial board, including hiring members, setting policies, and verifying that they comply with industry standards. It requires working with the board to identify areas for development and developing training programs to improve their skills.</li>
 
                 
-                <li><b>Manage the Peer-Review Process</b>: The publisher is responsible for managing the journal's peer-review process. It involves choosing reviewers, keeping track of the review process, and talking to authors and reviewers.</li>
+                <li class="responsibility"><b>Manage the Peer-Review Process</b>: The publisher is responsible for managing the journal's peer-review process. It involves choosing reviewers, keeping track of the review process, and talking to authors and reviewers.</li>
 
                 
-                <li><b>Publication Ethics</b>: Publishers are responsible for making sure that all submitted manuscripts follow ethical rules. This includes checking the research to see if it meets ethical standards, making sure that writers have all the permissions and licenses they need, and looking for plagiarism or other types of fraud.</li>
+                <li class="responsibility"><b>Publication Ethics</b>: Publishers are responsible for making sure that all submitted manuscripts follow ethical rules. This includes checking the research to see if it meets ethical standards, making sure that writers have all the permissions and licenses they need, and looking for plagiarism or other types of fraud.</li>
 
                 
-                <li><b>Publish Papers</b>: The publisher is in charge of publishing manuscripts in the academic journal. It requires copyediting and proofreading of the papers that will be published.</li>
+                <li class="responsibility"><b>Publish Papers</b>: The publisher is in charge of publishing manuscripts in the academic journal. It requires copyediting and proofreading of the papers that will be published.</li>
 
-                <li><b>Indexing</b>: The publisher indexes papers from journals, enabling researchers and other readers to locate the manuscripts.</li>
+                <li class="responsibility"><b>Indexing</b>: The publisher indexes papers from journals, enabling researchers and other readers to locate the manuscripts.</li>
                 
                 <h4>Peer Review Policy</h4>
                 <p class="details">Through peer review, professionals in the field review manuscripts sent to a publication. The point of peer review is to make sure that the research is of an excellent standard and that its findings are significant. Reviewers should look at the following:
                 </p>
-                <li>The originality of the research</li>
-                <li>The soundness of the methodology</li>
-                <li>The clarity of the writing</li>
-                <li>The significance of the findings</li>
+                <li class="responsibility">The originality of the research</li>
+                <li class="responsibility">The soundness of the methodology</li>
+                <li class="responsibility">The clarity of the writing</li>
+                <li class="responsibility">The significance of the findings</li>
 
                 <p class="details">Peer reviewers provide feedback to authors on how to improve their manuscripts. The journal editor makes the final decision on whether to accept or reject a manuscript.
                 </p>
@@ -123,25 +123,25 @@
                 <h4>Peer Review Selection</h4>
                 <p class="details">Here are some of the factors that the TRR considers when selecting peer reviewers:</p>
 
-                <li>Expertise: Peer reviewers are usually chosen based on how much they know about the manuscript's field. It means that they have written research on the subject, know what other research has been done, and can judge the quality of the study.</li>
+                <li class="responsibility">Expertise: Peer reviewers are usually chosen based on how much they know about the manuscript's field. It means that they have written research on the subject, know what other research has been done, and can judge the quality of the study.</li>
                 
-                <li>Confidentiality: Peer reviewers must maintain confidentiality about the authors' identity and refrain from discussing the manuscript with anyone else, including colleagues, students, or friends.</li>
+                <li class="responsibility">Confidentiality: Peer reviewers must maintain confidentiality about the authors' identity and refrain from discussing the manuscript with anyone else, including colleagues, students, or friends.</li>
                 
-                <li>Timeliness: Peer reviewers have to finish their reviews promptly, taking no more than a few weeks to review the manuscript.</li>
+                <li class="responsibility">Timeliness: Peer reviewers have to finish their reviews promptly, taking no more than a few weeks to review the manuscript.</li>
                 
-                <li>Objectivity: Peer reviewers must look at the manuscripts fairly and objectively without letting their ideas or biases affect their reviews.</li>
+                <li class="responsibility">Objectivity: Peer reviewers must look at the manuscripts fairly and objectively without letting their ideas or biases affect their reviews.</li>
                 
-                <li>Feedback: Peer reviewers must provide specific, helpful, and actionable feedback to authors on improving their manuscripts.</li>
+                <li class="responsibility">Feedback: Peer reviewers must provide specific, helpful, and actionable feedback to authors on improving their manuscripts.</li>
                     
                 <h4>Final Decision</h4>
 
                 <p class="details">The editorial team, consisting of the editor or the editorial board, evaluates the feedback provided by the peer reviewers and later arrives at a decision. The following are the most often chosen options made by people:</p>
 
-                <li><b>Accept without any changes (approval)</b>: the paper will be published in its original form by the publication.
-                <li><b>Accept with minor revisions (approval)</b>: The journal will publish the paper, but the author needs to make a few small changes.
-                <li><b>Accept with major changes (conditional approval)</b>: The paper will be published in the journal if the authors make the changes recommended by the reviewers and editors.
-                <li><b>Revise and resubmit (conditional rejection)</b>: If the authors make major changes, the journal will look at the work again in the next round of decisions.
-                <li><b>Reject the paper (outright rejection)</b>: the journal is unlikely to publish it or look at it again, even if the authors make major changes.</p>
+                <li class="responsibility"><b>Accept without any changes (approval)</b>: the paper will be published in its original form by the publication.
+                <li class="responsibility"><b>Accept with minor revisions (approval)</b>: The journal will publish the paper, but the author needs to make a few small changes.
+                <li class="responsibility"><b>Accept with major changes (conditional approval)</b>: The paper will be published in the journal if the authors make the changes recommended by the reviewers and editors.
+                <li class="responsibility"><b>Revise and resubmit (conditional rejection)</b>: If the authors make major changes, the journal will look at the work again in the next round of decisions.
+                <li class="responsibility"><b>Reject the paper (outright rejection)</b>: the journal is unlikely to publish it or look at it again, even if the authors make major changes.</p>
 
 
 
