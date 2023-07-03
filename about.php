5f0a0e309638b729837ca6d7d10f8a3be2c17d4c
trxsrn <trixielavadiasoriano@gmail.com>
2023-07-03 12:09:22 +0800

    Update changes

---
+++ about.php
@@ -0,0 +1,152 @@
+<?php include 'navigation.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <link rel="stylesheet" href="css/about.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+
+    
+
+</head>
+<body>
+    <div class="content">
+        <div class="background">
+            <h1 class="title">THE RIZALIAN REVIEW </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit rerum quod nesciunt sed dolorem ab ipsa eaque facere quasi delectus optio deleniti dolorum, dolores, ullam enim unde! Veritatis, officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum labore reiciendis voluptatibus. Quod ut in, reiciendis laboriosam eaque, incidunt provident vero ipsum fuga cumque tempora eius repellendus suscipit eum amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias provident vel cum! Quibusdam explicabo beatae delectus praesentium minus? Animi id amet autem debitis ullam, fugiat sint dignissimos obcaecati aliquid enim.</p>
+        </div>
+        <div class="team">
+            <h2 class="headers">TEAM</h2>
+                <div class="members">
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="css/img/RDC logo.png">
+                        <h5>Trixie L. Soriano </h5>
+                        <h6>Position</h6>
+                        <div class="connect">
+                            <i class="fa fa-facebook-f" style="font-size: 15px; padding: 10px;"></i>
+                            <i class="material-icons" style="font-size: 15px; padding: 10px;">email</i>
+                            <i class="fa fa-linkedin" style="font-size: 15px; padding: 10px;"></i>
+                        </div>
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+</body>
+</html>
