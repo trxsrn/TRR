79bffae51a268896500be831799553efe0f4ae74
trxsrn <trixielavadiasoriano@gmail.com>
2023-07-21 16:54:45 +0800

    Update changes

--- js/switch.js
+++ js/switch.js
@@ -1,17 +1,26 @@
-$( document ).ready(function() {
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form.signup-link a");
 
-    var usertype = "";
-    //For Switching User Login Form from Author to Researcher 
-    $( ".btn-left" ).click(function() {
-      $("#swiper").css("left","0%");
-      usertype="Author";
-    });
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
 
-    $( ".btn-right" ).click(function() {
-        $("#swiper").css("left","50%");
-        usertype="Reviewer";
-      });
-    //Backend programmer can use this 'usertype' variable 
-    //to determine the if user is researcher/reviewer. -Ced:)
-});
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+
+signupLink.onclick = () => {
+  const activeForm = document.querySelector("input[name='slide']:checked").id;
+  if (activeForm === "login") {
+     window.location.href = "register_author.php";
+  } else if (activeForm === "signup") {
+      window.location.href = "register_reviewer.php";
+  }
+  return false;
+};
 
--- login.php
+++ login.php
@@ -103,34 +103,6 @@
             </div>
          </div>
       </div>
-
-      <script>
-         const loginText = document.querySelector(".title-text .login");
-         const loginForm = document.querySelector("form.login");
-         const loginBtn = document.querySelector("label.login");
-         const signupBtn = document.querySelector("label.signup");
-         const signupLink = document.querySelector("form.signup-link a");
-
-         signupBtn.onclick = () => {
-            loginForm.style.marginLeft = "-50%";
-            loginText.style.marginLeft = "-50%";
-         };
-
-         loginBtn.onclick = () => {
-            loginForm.style.marginLeft = "0%";
-            loginText.style.marginLeft = "0%";
-         };
-
-         signupLink.onclick = () => {
-            const activeForm = document.querySelector("input[name='slide']:checked").id;
-            if (activeForm === "login") {
-               window.location.href = "register_author.php";
-            } else if (activeForm === "signup") {
-               window.location.href = "register_reviewer.php";
-            }
-            return false;
-         };
-
-   </script>
+      <script src="switch.js"></script>
    </body>
 </html>
